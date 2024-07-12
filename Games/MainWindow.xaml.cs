b390f0c3daf03b15c079b0c93e638d0f9fcb5a8f
WainDev <123101928+WainDev@users.noreply.github.com>
2024-07-12 16:12:00 +0300

    version 0.2

--- Games/MainWindow.xaml.cs
+++ Games/MainWindow.xaml.cs
@@ -48,4 +48,30 @@ public partial class MainWindow : Window
             animalEmoji.RemoveAt(index);
         }
     }
+
+    TextBlock lastTextBlockClicked;
+
+    bool findingMatch = false;
+
+    private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        TextBlock textBlock = (TextBlock)sender;
+
+        if (findingMatch == false)
+        {
+            textBlock.Visibility = Visibility.Hidden;
+            lastTextBlockClicked = textBlock;
+            findingMatch = true;
+        } 
+        else if (textBlock.Text == lastTextBlockClicked.Text)
+        {
+            textBlock.Visibility= Visibility.Hidden;
+            findingMatch= false;
+        }
+        else
+        {
+            lastTextBlockClicked.Visibility= Visibility.Visible; 
+            findingMatch = false;
+        }
+    }
 }
