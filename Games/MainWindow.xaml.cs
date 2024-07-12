a28b5f6146d9af8980b452f16498fa94c0c5d279
WainDev <123101928+WainDev@users.noreply.github.com>
2024-07-12 15:32:11 +0300

    Добавьте файлы проекта.

---
+++ Games/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Games;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<string>()
+            {
+                "🐱‍🐉", "🐱‍🐉",
+                "👽", "👽",
+                "🧜‍♀️", "🧜‍♀️",
+                "🧛‍♀️", "🧛‍♀️",
+                "🐗", "🐗",
+                "🙈", "🙈",
+                "🦊", "🦊",
+                "🐶", "🐶",
+                "🐹", "🐹",
+                "👻", "👻",
+            };
+
+        Random rnd = new Random();
+
+        foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+        {
+            int index = rnd.Next(animalEmoji.Count);
+
+            string nextEmoji = animalEmoji[index];
+
+            textBlock.Text = nextEmoji;
+
+            animalEmoji.RemoveAt(index);
+        }
+    }
+}
