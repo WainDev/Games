e4b83ce6ee51879d6ba7ef7fe842cd6672f58a74
WainDev <123101928+WainDev@users.noreply.github.com>
2024-07-12 15:40:37 +0300

    Тестирование

--- Games/MainWindow.xaml.cs
+++ Games/MainWindow.xaml.cs
@@ -45,7 +45,7 @@ public partial class MainWindow : Window
 
             textBlock.Text = nextEmoji;
 
-            animalEmoji.RemoveAt(index);ss
+            animalEmoji.RemoveAt(index);
         }
     }
 }
